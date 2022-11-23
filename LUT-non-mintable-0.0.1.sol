ed305ebd6f90c1ac1cbb0de7a2d61bfdec22a556
rogupoletti <54130148+rogupoletti@users.noreply.github.com>
2022-11-23 00:00:12 -0300

    Update LUT-non-mintable-0.0.1.sol
    Added function to mint the predfined number of tokens if the predefined time has elapsed.

--- LUT-non-mintable-0.0.1.sol
+++ LUT-non-mintable-0.0.1.sol
@@ -5,7 +5,25 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
+
+    uint256 public initialTime;
+
     constructor() ERC20("Libertarian Universe Token", "LUT") {
-      _mint(msg.sender, 600000 * 10 ** decimals());
+        initialTime = block.timestamp;
+        _mint(msg.sender, 600000 * 10 ** decimals());
     }
+
+    function mintOnTime() public {
+        uint256 elapsedTime = block.timestamp - initialTime;
+        if(elapsedTime >= 365 days && this.totalSupply() == 600000 * 10 ** decimals()){
+            _mint(this.owner(), 150000 * 10 ** decimals());
+        }
+        if(elapsedTime >= 730 days && this.totalSupply() == 750000 * 10 ** decimals()){
+            _mint(this.owner(), 150000 * 10 ** decimals());
+        }
+        if(elapsedTime >= 1095 days && this.totalSupply() == 900000 * 10 ** decimals()){
+            _mint(this.owner(), 100000 * 10 ** decimals());
+        }
+    }
+
 }
