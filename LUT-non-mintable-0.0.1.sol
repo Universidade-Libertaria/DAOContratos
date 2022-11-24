f04c6b111a98b83a622533ece96d24881ad87ae5
rogupoletti <54130148+rogupoletti@users.noreply.github.com>
2022-11-24 14:00:05 -0300

    Update LUT-non-mintable-0.0.1.sol
    Adjusted so it doesn1t consume too much gas and that the error messages are correctly returned.

--- LUT-non-mintable-0.0.1.sol
+++ LUT-non-mintable-0.0.1.sol
@@ -1,29 +1,45 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
+contract LibertarianUniverseToken is ERC20, Ownable {
 
     uint256 public initialTime;
+    uint256 public tranche;
 
     constructor() ERC20("Libertarian Universe Token", "LUT") {
         initialTime = block.timestamp;
+        tranche = 0;
         _mint(msg.sender, 600000 * 10 ** decimals());
     }
 
-    function mintOnTime() public {
+    function mintTranche1() public {
         uint256 elapsedTime = block.timestamp - initialTime;
-        if(elapsedTime >= 365 days && this.totalSupply() == 600000 * 10 ** decimals()){
-            _mint(this.owner(), 150000 * 10 ** decimals());
-        }
-        if(elapsedTime >= 730 days && this.totalSupply() == 750000 * 10 ** decimals()){
-            _mint(this.owner(), 150000 * 10 ** decimals());
-        }
-        if(elapsedTime >= 1095 days && this.totalSupply() == 900000 * 10 ** decimals()){
-            _mint(this.owner(), 100000 * 10 ** decimals());
-        }
+        require(elapsedTime >= 365 days, "It's not time yet.");
+        require(this.totalSupply() == 600000 * 10 ** decimals(), "Already minted.");
+        _mint(this.owner(), 150000 * 10 ** decimals());
+        tranche++;  
     }
+
+    function mintTranche2() public {
+        uint256 elapsedTime = block.timestamp - initialTime;
+        require(tranche >= 1, "You need to mint Tranche 1 to mint this one.");
+        require(elapsedTime >= 730 days, "It's not time yet.");
+        require(this.totalSupply() == 750000 * 10 ** decimals(), "Already minted.");
+        _mint(this.owner(), 150000 * 10 ** decimals());
+        tranche++;
+    }
+
+    function mintTranche3() public {
+        uint256 elapsedTime = block.timestamp - initialTime;
+        require(tranche >= 2, "You need to mint Tranche 2 to mint this one.");
+        require(elapsedTime >= 1095 days, "It's not time yet.");
+        require(this.totalSupply() == 900000 * 10 ** decimals(), "Already minted.");
+        _mint(this.owner(), 100000 * 10 ** decimals());
+        tranche++;        
+    }
+
 
 }
